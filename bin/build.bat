086a69d6a7ecb0a6bb46ef78b8c0f8430885a5ed
Ethan Lu <elu@nevada.unr.edu>
2018-09-21 19:54:09 -0700

    Add bhtsne implementation, vscode environment, nlopt stuff

--- bin/build.bat
+++ bin/build.bat
@@ -4,11 +4,12 @@ if not exist w:\build mkdir w:\build
 pushd w:\build
 
 set CompilerFlags=%1 -MTd -nologo -Oi -W4 -MP -Z7 -FC -EHsc -Iw:\deps\ -Iw:\src\ -Feviz
-set LinkerFlags=/NODEFAULTLIB:MSVCRT /SUBSYSTEM:console /LIBPATH:w:\deps\SDL2\lib\ SDL2.lib SDL2main.lib opengl32.lib user32.lib gdi32.lib opengl32.lib kernel32.lib
-set Files=w:\src\build.cpp w:\src\gl.cpp w:\deps\glad\glad.c w:\deps\imgui\imgui_compile.cpp
+set LinkerFlags=/NODEFAULTLIB:MSVCRT /SUBSYSTEM:console /LIBPATH:w:\deps\SDL2\lib\ /LIBPATH:w:\deps\nlopt\lib\ SDL2.lib SDL2main.lib nlopt.lib opengl32.lib user32.lib gdi32.lib opengl32.lib kernel32.lib
+set Files=w:\src\build.cpp w:\src\gl.cpp w:\deps\glad\glad.c w:\deps\imgui\imgui_compile.cpp w:\deps\bhtsne\tsne.cpp w:\deps\bhtsne\sptree.cpp
 
 cl %CompilerFlags% %Files% /link %LinkerFlags%
 
 xcopy w:\deps\SDL2\lib\SDL2.dll w:\build\ /C /Y  > NUL 2> NUL
+xcopy w:\deps\nlopt\lib\nlopt.dll w:\build\ /C /Y  > NUL 2> NUL
 
 popd
--- src/build.cpp
+++ src/build.cpp
@@ -9,13 +9,108 @@
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_sdl.h>
 #include <imgui/imgui_impl_opengl3.h>
+
 #include <iostream>
+#include <iomanip>
+#include <vector>
+
+#include <bhtsne/tsne.h>
+#include <cfloat>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+
+#include <nlopt/nlopt.hpp>
 
 using std::cout;
 using std::endl;
 
 SDL_Window* window = null;
 SDL_GLContext gl_context = null;
+
+f64 myfunc(u32, const f64 *x, f64 *grad, void*)
+{
+	if (grad) {
+		grad[0] = 0.0;
+		grad[1] = 0.5 / sqrt(x[1]);
+	}
+	return sqrt(x[1]);
+}
+typedef struct {
+	f64 a, b;
+} my_constraint_data;
+
+f64 myconstraint(u32, const f64 *x, f64 *grad, void *data)
+{
+	my_constraint_data *d = (my_constraint_data *)data;
+	f64 a = d->a, b = d->b;
+	if (grad) {
+		grad[0] = 3 * a * (a*x[0] + b) * (a*x[0] + b);
+		grad[1] = -1.0;
+	}
+	return ((a*x[0] + b) * (a*x[0] + b) * (a*x[0] + b) - x[1]);
+}
+
+void test_nlopt() {
+
+	nlopt::opt opt(nlopt::LD_MMA, 2);
+	std::vector<f64> lb(2);
+	lb[0] = -HUGE_VAL; lb[1] = 0;
+	opt.set_lower_bounds(lb);
+	opt.set_min_objective(myfunc, NULL);
+	my_constraint_data data[2] = { {2,0}, {-1,1} };
+	opt.add_inequality_constraint(myconstraint, &data[0], 1e-8);
+	opt.add_inequality_constraint(myconstraint, &data[1], 1e-8);
+	opt.set_xtol_rel(1e-4);
+	std::vector<f64> x(2);
+	x[0] = 1.234; x[1] = 5.678;
+	f64 minf;
+
+	try {
+		opt.optimize(x, minf);
+		std::cout << "found minimum at f(" << x[0] << "," << x[1] << ") = "
+			<< std::setprecision(10) << minf << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "nlopt failed: " << e.what() << std::endl;
+	}
+}
+void test_bhtsne() {
+// Define some variables
+	int origN, N, D, no_dims, max_iter, *landmarks;
+	double perc_landmarks;
+	double perplexity, theta, *data;
+    int rand_seed = -1;
+    TSNE* tsne = new TSNE();
+
+    // Read the parameters and the dataset
+	if(tsne->load_data(&data, &origN, &D, &no_dims, &theta, &perplexity, &rand_seed, &max_iter)) {
+
+		// Make dummy landmarks
+        N = origN;
+        int* landmarks = (int*) malloc(N * sizeof(int));
+        if(landmarks == NULL) { printf("Memory allocation failed!\n"); exit(1); }
+        for(int n = 0; n < N; n++) landmarks[n] = n;
+
+		// Now fire up the SNE implementation
+		double* Y = (double*) malloc(N * no_dims * sizeof(double));
+		double* costs = (double*) calloc(N, sizeof(double));
+        if(Y == NULL || costs == NULL) { printf("Memory allocation failed!\n"); exit(1); }
+		tsne->run(data, N, D, Y, no_dims, perplexity, theta, rand_seed, false, max_iter);
+
+		// Save the results
+		tsne->save_data(Y, landmarks, costs, N, no_dims);
+
+        // Clean up the memory
+		free(data); data = NULL;
+		free(Y); Y = NULL;
+		free(costs); costs = NULL;
+		free(landmarks); landmarks = NULL;
+    }
+    delete(tsne);
+}
 
 enum class mode {
 	cam, 
@@ -48,7 +143,7 @@ void plt_setup() {
 
 	glEnable(GL_DEBUG_OUTPUT);
 	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-	glDebugMessageCallback(debug_proc, 0);
+	glDebugMessageCallback((GLDEBUGPROC)debug_proc, 0);
 	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, GL_TRUE);
 }
 
@@ -83,6 +178,8 @@ void begin_frame() {
 
 int main(int, char**) {
 
+	test_nlopt();
+	test_bhtsne();
 	plt_setup();
 
 	shader s; s.load("mesh.v", "mesh.f");
